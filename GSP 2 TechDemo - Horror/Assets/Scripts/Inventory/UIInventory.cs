25e46ab1377b601b67be5a2f452960f1c089f798
Afronomical <66644410+Afronomical@users.noreply.github.com>
2023-11-06 16:32:09 +0000

    Final

--- GSP 2 TechDemo - Horror/Assets/Scripts/Inventory/UIInventory.cs
+++ GSP 2 TechDemo - Horror/Assets/Scripts/Inventory/UIInventory.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using JetBrains.Annotations;
+using CodeMonkey.Utils;
 
 public class UIInventory : MonoBehaviour
 {
     private Inventory inventory;
     private Transform itemSlotContainer;
     private Transform itemSlotTemplate;
+  
 
     private void Awake()
     {
         itemSlotContainer = transform.Find("itemSlotContainer");
         itemSlotTemplate = itemSlotContainer.Find("itemSlotTemplate");
         Debug.Log(itemSlotTemplate.name);
-    } 
+    }
+    private void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
     public void SetInventory(Inventory inventory)
     {
         this.inventory = inventory;
@@ -22,6 +30,11 @@ public class UIInventory : MonoBehaviour
         RefreshInventoryItems();
     }
 
+    private void Update()
+    {
+        
+        
+    }
     private void Inventory_OnItemListChanged(object sender, System.EventArgs e)
     {
         RefreshInventoryItems();
@@ -35,28 +48,59 @@ public class UIInventory : MonoBehaviour
 
         foreach (Transform child in itemSlotContainer)
         {
-            if (child == itemSlotTemplate)
-                continue;
+            if (child == itemSlotTemplate) continue;
 
             Destroy(child.gameObject);
 
         }
 
         foreach (Item item in inventory.GetItemList())
-        {
+        {   
             
 
             RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
             itemSlotRectTransform.gameObject.SetActive(true);
+
+
+            itemSlotRectTransform.GetComponent<Button_UI>().ClickFunc = () =>
+            {
+                inventory.UseItem(item);
+            };
+
+            itemSlotRectTransform.GetComponent<Button_UI>().MouseRightClickFunc = () =>
+                {
+                    /*GameObject GO = Instantiate(item.itemPhysical);
+                    GO.GetComponent<PickupItem>().itemAmount = item.amount;*/
+                    inventory.RemoveItem(item);
+                };
+            
+            
+
+          
+
+
             itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, y * itemSlotCellSize);
             Image image = itemSlotRectTransform.Find("Image").GetComponent<Image>();
+
             image.sprite = item.GetSprite();
+            TextMeshProUGUI uiText = itemSlotRectTransform.Find("text").GetComponent<TextMeshProUGUI>();
+            if(item.amount > 1)
+            {
+                uiText.SetText(item.amount.ToString());
+            }
+            else
+            {
+                uiText.SetText("");
+            }
+
             x++;
-            if(x > 4)
+            if(x >= 4)
             {
                 x = 0;
-                y++;
+                y--;
             }
+
+            
         }
     }
 }
